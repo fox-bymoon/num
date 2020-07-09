b276ee8ca79f283a06803791e62c789f7c74e0e1
ex_renbaokun <ex_renbaokun@axatp.com>
2020-07-09 16:28:17 +0800

    初次整理进行发版

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,22 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.Info;
 import com.example.demo.dao.UserDao;
-import com.example.demo.entity.User;
 import com.example.demo.service.RedisService;
+import com.example.demo.util.JsonUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
     private final UserDao userDao;
     private final RedisService redisService;
+    private final Info info;
 
-    public TestController(UserDao userDao, RedisService redisService) {
+    public TestController(UserDao userDao, RedisService redisService, Info info) {
         this.userDao = userDao;
         this.redisService = redisService;
+        this.info = info;
     }
 
-    @RequestMapping("/sayHello")
+   /* @RequestMapping("/sayHello")
     public String syaHello(){
         return "你好";
     }
@@ -25,10 +30,10 @@ public class TestController {
         User all = userDao.findAll();
         System.out.println(all);
         return "ok";
-    }
+    }*/
 
     //登陆
-    @RequestMapping("/login")
+    /*@RequestMapping("/login")
     public String userLogin(User user){
         User userInfo = userDao.userLogin(user);
         if (null != userInfo){
@@ -49,5 +54,14 @@ public class TestController {
             return "true";
         }
         return "false";
+    }*/
+    //查询数据当天的
+    @RequestMapping("/findAllInfo")
+    public String getInfo(){
+        Map<String, Object> infos =info.findInfo();
+        String mid = infos.get("time_h")+"";
+        String good_number_total = infos.get("good_number_total")+"";
+        System.out.println(info);
+        return JsonUtil.getListToJson(infos);
     }
 }
---
+++ src/main/java/com/example/demo/dao/Info.java
@@ -0,0 +1,8 @@
+package com.example.demo.dao;
+
+import java.util.Map;
+
+public interface Info {
+    //获取数据
+    Map<String,Object> findInfo();
+}
